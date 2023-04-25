384777985fff1f922b075ec1e9a61c7ef88f38b8
chiomanwocha <nwochachioma1998@gmail.com>
2023-04-25 07:36:04 -0700

    modified fiel

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,25 @@
 #include "main.h"
 
+
 /**
- * main - print _putchar, followed by new line
- * Return: 0
+ * main - entry poiny
+ *
+ * Discription: Prints _putchar
+ *
+ * Return: 0 (success)
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-	return (0);
+return (0);
 }
