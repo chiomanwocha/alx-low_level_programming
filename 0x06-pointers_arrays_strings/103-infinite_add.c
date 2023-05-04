7f179500a89a9dee898c9f99543ed36a9e8c5122
Nwocha <77916165+chiomanwocha@users.noreply.github.com>
2023-05-04 21:11:03 +0100

    modified function

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,62 +1,47 @@
 #include "main.h"
 
 /**
- * reverse array
- */
-void rev_string(char *n)
-{
-    int i = 0, j = 0;
-    char temp;
-
-    while (*(n + i) != '\0')
-    {
-        i++;
-    }
-    i--;
-
-    for (j = 0; j < i; j++, i--)
-    {
-        temp = *(n + j);
-        *(n + j) = *(n + i);
-        *(n + i) = temp;
-    }
-}
-
-/**
- * add 2 numbers together
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for result
+ * @size_r: buffer size
+ * ahhh! Crazy task! Expand your knowledge
+ * Return: address of r or 0
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int i = 0, j = 0, carry = 0;
+	int i, j, k, l, m, n;
 
-    while (*(n1 + i) != '\0' || *(n2 + j) != '\0')
-    {
-        int val1 = i < strlen(n1) ? *(n1 + i++) - '0' : 0;
-        int val2 = j < strlen(n2) ? *(n2 + j++) - '0' : 0;
-        carry += val1 + val2;
-        if (carry > 9)
-        {
-            *(r++) = (carry - 10) + '0';
-            carry = 1;
-        }
-        else
-        {
-            *(r++) = carry + '0';
-            carry = 0;
-        }
-        if (r - size_r == r)
-        {
-            return 0;
-        }
-    }
-
-    if (carry > 0)
-    {
-        *(r++) = carry + '0';
-    }
-    *(r++) = '\0';
-
-    rev_string(r - (r - size_r > r - 1 ? r - 1 : r - size_r));
-
-    return (size_r < r - r ? 0 : r - (r - size_r > r - 1 ? r - 1 : r - size_r));
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
+		return (0);
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	{
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
+			break;
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
+	}
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
+	{
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
+	}
+	return (r);
 }
