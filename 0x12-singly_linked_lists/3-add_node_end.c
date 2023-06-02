e85e58018260caff34ccdb471f80dad8395a371b
Nwocha <77916165+chiomanwocha@users.noreply.github.com>
2023-06-02 15:49:17 +0100

    fixed betty issues

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,9 +13,9 @@ unsigned int _strlen(char *str)
 	unsigned int length;
 
 	for (length = 0; str[length]; length++)
-    {
-        /* Empty loop body */
-    }
+	{
+		/* Empty loop body */
+	}
 	return (length);
 }
 
@@ -32,25 +32,31 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	if (str == NULL)
 		return (NULL);
+
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
+
 	new_node->len = _strlen(new_node->str);
 	new_node->next = NULL;
+
 	if (*head == NULL)
 	{
 		*head = new_node;
-		return new_node;
+		return (*head);
 	}
+
 	tmp = *head;
 	while (tmp->next)
 		tmp = tmp->next;
+
 	tmp->next = new_node;
 	return (new_node);
 }
