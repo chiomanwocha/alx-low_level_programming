9ff79bf70dd85a94b2309b0f5e53f176391411fb
Nwocha <77916165+chiomanwocha@users.noreply.github.com>
2023-06-02 15:50:59 +0100

    fixed betty issues

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,14 +10,16 @@ void free_list(list_t *head)
 {
 	if (head)
 	{
-        if (head->next)
-        {
-            free_list(head->next);
-        }
-        if (head->str)
-        {
-            free(head->str);
-        }
+		if (head->next)
+		{
+			free_list(head->next);
+		}
+
+		if (head->str)
+		{
+			free(head->str);
+		}
+
 		free(head);
 	}
 }
