0c57463f933e141f20de5a676f108d17717b76b9
chiomanwocha <nwochachioma1998@gmail.com>
2023-06-06 13:54:06 -0700

    fixes

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,98 +1,88 @@
 #include "lists.h"
+#include <stdio.h>
+
+size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
 
 /**
- * count_nodes_till_loop - Counts the number of unique nodes in the list before a loop.
- * @head: Pointer to the head pointer of the linked list.
- * Return: Number of unique nodes in the list before a loop.
+ * looped_listint_len - Counts the number of distinct nodes in a circular linked list.
+ * @head: A pointer to the head of the circular linked list to check.
+ *
+ * Return: If the list is not circular - 0.
+ *         Otherwise - the number of distinct nodes in the list.
  */
-int count_nodes_till_loop(const listint_t *head)
+size_t looped_listint_len(const listint_t *head)
 {
-	int count = 0;
-	const listint_t *turtle, *hare;
+	const listint_t *tortoise, *hare;
+	size_t nodes = 1;
 
-	turtle = hare = head;
+	if (head == NULL || head->next == NULL)
+		return (0);
 
-	while (turtle != NULL && hare != NULL)
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare)
 	{
-		turtle = turtle->next;
-		hare = hare->next->next;
-		count++;
-
-		if (turtle == hare)
+		if (tortoise == hare)
 		{
-			turtle = head;
-			while (turtle != hare)
+			tortoise = head;
+			while (tortoise != hare)
 			{
-				turtle = turtle->next;
+				nodes++;
+				tortoise = tortoise->next;
 				hare = hare->next;
-				count++;
 			}
-			return (count);
+
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+			}
+
+			return (nodes);
 		}
+
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
 	}
+
 	return (0);
 }
 
 /**
- * loop - Checks if there's a loop in the linked list.
- * @head: Pointer to the head pointer of the linked list.
- * Return: 0 if no loop, 1 if loop.
- */
-int loop(const listint_t *head)
-{
-	const listint_t *turtle, *hare;
-
-	turtle = hare = head;
-
-	while (turtle != NULL && hare != NULL)
-	{
-		turtle = turtle->next;
-		hare = hare->next->next;
-
-		if (turtle == hare)
-			return (1);
-	}
-	return (0);
-}
-
-/**
- * print_listint_safe - Prints the linked list with addresses.
- * @head: Pointer to the head pointer of the linked list.
- * Return: Number of nodes in the list, or exits with code 98 if failed.
+ * print_listint_safe - Safely prints the elements of a linked list,
+ * even if it contains a circular reference.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Return: The number of nodes in the linked list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	int count = 0;
-	int loop_found;
-	size_t num_nodes = 0;
-	const listint_t *tmp;
+	size_t nodes, index = 0;
 
-	if (head == NULL)
-		exit(98);
+	nodes = looped_listint_len(head);
 
-	loop_found = loop(head);
-
-	if (loop_found == 1) /* Print up to the last node before the loop if a loop is present */
+	if (nodes == 0)
 	{
-		count = count_nodes_till_loop(head);
-		for (loop_found = 0; loop_found < count; loop_found++)
+		for (; head != NULL; nodes++)
 		{
-			printf("[%p] %d\n", (void *)tmp, tmp->n);
-			num_nodes += 1;
-			tmp = tmp->next;
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
 	}
-	else if (loop_found == 0) /* Print the list regularly up to NULL if no loop is present */
+	else
 	{
-		tmp = head;
-		while (tmp != NULL)
+		for (index = 0; index < nodes; index++)
 		{
-			printf("[%p] %d\n", (void *)tmp, tmp->n);
-			num_nodes += 1;
-			tmp = tmp->next;
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
+
+		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
 
-	return (num_nodes);
+	return (nodes);
 }
 
