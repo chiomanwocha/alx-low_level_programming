0fbc22e192b5305efd179b597b964aa7bbf38ef6
chiomanwocha <nwochachioma1998@gmail.com>
2023-06-08 13:31:12 -0700

    added a function that prints the binary representation of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _power - Calculates the power of a number.
+ * @base: The base number.
+ * @pow: The exponent.
+ *
+ * Return: The result of base raised to the power of pow.
+ */
+unsigned long int _power(unsigned int base, unsigned int pow)
+{
+	unsigned long int num;
+	unsigned int i;
+
+	num = 1;
+	for (i = 1; i <= pow; i++)
+		num *= base;
+	return (num);
+}
+
+/**
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number to print in binary.
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int dev, result;
+	char flag;
+
+	flag = 0;
+	dev = _power(2, sizeof(unsigned long int) * 8 - 1);
+
+	while (dev != 0)
+	{
+		result = n & dev;
+		if (result == dev)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag == 1 || dev == 1)
+		{
+			_putchar('0');
+		}
+		dev >>= 1;
+	}
+}
+
