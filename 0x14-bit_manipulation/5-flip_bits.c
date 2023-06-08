f1df3f054c6c3b5420c8d725e3a7095aef3c19d7
chiomanwocha <nwochachioma1998@gmail.com>
2023-06-08 14:00:55 -0700

    fixes

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,31 +1,27 @@
 #include "main.h"
 
 /**
- * flip_bits - Counts the number of bits needed to flip to convert
- *             one number to another.
+ * flip_bits - Counts the number of bits to change
+ *             to get from one number to another.
+ *
  * @n: The first number.
  * @m: The second number.
  *
- * Return: The number of bits needed to flip.
+ * Return: The number of bits to change.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int difference, result;
-	unsigned int count, i;
+	int i, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
 
-	difference = n ^ m;  /* XOR the two numbers to get the differing bits */
-	count = 0;  /* Initialize the count of differing bits to 0 */
-
-	/* Iterate through each bit of the difference */
-	for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
+	for (i = 63; i >= 0; i--)
 	{
-		result = 1 << i;  /* Shift 1 to the current bit position */
-
-		/* Check if the bit at the current position is set in the difference */
-		if (result == (difference & result))
-			count++;  /* Increment the count if the bit is set */
+		current = exclusive >> i;
+		if (current & 1)
+			count++;
 	}
 
-	return (count);  /* Return the count of differing bits */
+	return (count);
 }
 
